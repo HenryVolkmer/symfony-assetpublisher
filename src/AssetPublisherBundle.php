d05428360ee6180d6af980e7b099e3fceea1c92a
Henry Volkmer <henry.volkmer@mdo-ito.de>
2026-03-17 15:03:44 +0100

    - [BUG] fix Returntype

--- src/AssetPublisherBundle.php
+++ src/AssetPublisherBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AssetPublisherBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
     }
 }
